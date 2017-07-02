d5a41f2c4abe2843490443eda1382a448be8d0a7
Angela <yim4real.design@gmail.com>
2017-07-02 18:25:19 -0400

    Update shapes.js

--- shapes.js
+++ shapes.js
@@ -5,7 +5,7 @@ var svg = d3.select("body").append("svg").attr("height","100%").attr("width","10
 svg.selectAll("rect")
   .data(dataArray)
   .enter().append("rect")
-	.attr("height",function(d,i){ return d*15; })
+	.attr("height",function(d,i){ return d*7; })
 	.attr("width","50")
 	.attr("fill","cornflowerblue")
 	.attr("x",function(d,i){ return 60*i; })
@@ -15,6 +15,6 @@ var newX = 300;
 svg.selectAll("circle")
   .data(dataArray)
   .enter().append("circle")
-	.attr("cx",function(d,i){ newX+=(d*12)+(i*20); return newX; })
+	.attr("cx",function(d,i){ newX+=(d*8)+(i*20); return newX; })
 	.attr("cy","100")
 	.attr("r",function(d,i){ return d*3; });
