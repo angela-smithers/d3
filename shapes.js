bb9a6cd0e6c42d8e1d2484bccf038539a352721c
Angela <yim4real.design@gmail.com>
2017-07-02 19:55:27 -0400

    Update shapes.js

--- shapes.js
+++ shapes.js
@@ -16,8 +16,8 @@ svg.selectAll("circle.first")
   .data(dataArray)
   .enter().append("circle")
 	.attr("class","first")
-	.attr("cx",function(d,i){ newX+=(d*6)+(i*14); return newX; })
-	.attr("cy","100")
+	.attr("cx",function(d,i){ newX+=(d*6)+(i*20); return newX; })
+	.attr("cy","80")
 	.attr("r",function(d,i){ return d*3; });
 
 var newX = 600;
@@ -27,4 +27,5 @@ svg.selectAll("ellipse.second")
 	.attr("class","second")
 	.attr("cx",function(d,i){ newX+=(d*8)+(i*20); return newX; })
 	.attr("cy","100")
-	.attr("r",function(d,i){ return d*3; });
+	.attr("rx",function(d){ return d*3; })
+	.attr("ry","30");
