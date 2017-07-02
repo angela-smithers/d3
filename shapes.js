9e9f0eba2792993e00bf1d1e415fbc4b8d882159
Angela <yim4real.design@gmail.com>
2017-07-02 17:43:30 -0400

    Update shapes.js

--- shapes.js
+++ shapes.js
@@ -5,4 +5,8 @@ var svg = d3.select("body").append("svg").attr("height","100%").attr("width","10
 svg.selectAll("rect")
   .data(dataArray)
   .enter().append("rect");
+					.attr("height","200")
+					.attr("width","50")
+					.attr("x","20")
+					.attr("y","20");
 
