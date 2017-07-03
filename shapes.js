3f753835ad5120772002ecffb63d86d6bb796d77
Angela <yim4real.design@gmail.com>
2017-07-02 20:03:35 -0400

    Update shapes.js

--- shapes.js
+++ shapes.js
@@ -25,7 +25,7 @@ svg.selectAll("ellipse.second")
   .data(dataArray)
   .enter().append("ellipse")
 	.attr("class","second")
-	.attr("cx",function(d,i){ newX+=(d*8)+(i*20); return newX; })
+	.attr("cx",function(d,i){ newX+=(d*8)+(i*12); return newX; })
 	.attr("cy","100")
 	.attr("rx",function(d){ return d*3; })
 	.attr("ry","30");
