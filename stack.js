0a5aa069113ec0bdaae9fb4dbc40811cf4408e8c
Angela <yim4real.design@gmail.com>
2017-08-13 18:53:44 -0400

    Update stack.js

--- stack.js
+++ stack.js
@@ -25,9 +25,9 @@ d3.xml("data2.xml").get(function(error,xml){
   var stack = d3.stack().keys(categories);
   
   var area = d3.area()
-                .x(function(d){ return d; })
-                .y0(function(d){ return d; })
-                .y1(function(d){ return d; });
+                .x(function(d){ return d.data.date; })
+                .y0(function(d){ return y(d[0]); })
+                .y1(function(d){ return y(d[1]); });
   
   var svg = d3.select("body").append("svg").attr("width","100%").attr("height","100%");
   var chartGroup = svg.append("g").attr("transform","translate("+margin.left+","+margin.top+")");
