3b7813322c8e8c7fb786e417c4b3de759b823643
Angela <yim4real.design@gmail.com>
2017-08-13 18:00:59 -0400

    Update stack.js

--- stack.js
+++ stack.js
@@ -1,1 +1,15 @@
+var parseDate = d3.timeParse("%Y")
 
+d3.xml("data2.xml").get(function(error,xml){
+  
+  xml = [].map.call(xml.querySelectorAll("dat"),function(d){
+    return {
+    date: parseDate(d.getAttribute("id")),
+      top: +d.querySelector("top").textContent,
+      middle: +d.querySelector("middle").textContent,
+      bottom: +d.querySelector("bottom").textContent
+    };
+  })
+  console.log(xml);
+
+});
