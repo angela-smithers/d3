4665be1455a368558e097bafb7a7c09214b11dda
Angela <yim4real.design@gmail.com>
2017-08-13 18:20:36 -0400

    Update stack.js

--- stack.js
+++ stack.js
@@ -2,6 +2,10 @@ var parseDate = d3.timeParse("%Y");
 
 d3.xml("data2.xml").get(function(error,xml){
   
+  var height = 200;
+  var width = 500;
+  var margin = {left: 50, right: 50, top: 40, bottom:0};
+  
   xml = [].map.call(xml.querySelectorAll("dat"),function(d){
     return {
     date: parseDate(d.getAttribute("id")),
@@ -10,6 +14,25 @@ d3.xml("data2.xml").get(function(error,xml){
       bottom: +d.querySelector("bottom").textContent
     };
   })
-  console.log(xml);
-
+  var x =d3.scaleTime()
+      .domain(d3.extend(xml,function(d){ return d.date; }))
+      .range([0,width]);
+  vay y = d3.scaleLinear()
+      .domain(0,d3.max(xml,function(d){ return d.top+d.middle+d.bottom; }))
+      .range([height,0]);
+  var categories = ['top','middle','bottom']
+  
+  var stack = d3.stack().keys(categories);
+  
+  var area = d3.area()
+                .x(function(d){ return d; })
+                .y0(function(d){ return d; })
+                .y1(function(d){ return d; });
+  
+  var svg = d3.select("body").append("svg").attr("width","100%").attr("height","100%");
+  var chartGroup = svg.append("g").attr("transform","translate("+margin.left+","+margin.top+")");
+  
+  var stacked = stack(xml);
+  console.log(stacked);
+  
 });
